f458b3a15f6f190b6588959b609d44e36113ce78
Ken Mickles <ken@37i.net>
2011-06-13 14:05:54 -0400

    password protect the app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate
+  
+  protected
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['HTTP_USERNAME'] && password == ENV['HTTP_PASSWORD']
+    end
+  end
+  
 end

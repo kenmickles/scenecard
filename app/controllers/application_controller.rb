243f36adf4d32d36aa8a272319cc37483987684f
Ken Mickles <ken@37i.net>
2015-08-26 12:32:27 -0400

    Re-enable basic auth for production env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  # before_filter :authenticate
+  before_filter :authenticate
   
   protected
 
   def authenticate
+    return unless Rails.env == "production"
+    
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV['HTTP_USERNAME'] && password == ENV['HTTP_PASSWORD']
     end
